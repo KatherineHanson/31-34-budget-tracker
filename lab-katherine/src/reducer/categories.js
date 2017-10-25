28c95abf7e8cc1086ce03dfa6adcf63c1c7eb0da
KatherineHanson <katherinemariehanson@gmail.com>
2017-10-25 15:21:55 -0700

    added middleware

--- lab-katherine/src/reducer/categories.js
+++ lab-katherine/src/reducer/categories.js
@@ -1,14 +1,20 @@
 const emptyState = []
 
+export const validateCategory = (category) => {
+  if(!category.name || !category.amount)
+    throw new Error('category expected a name and amount')
+}
+
 export default (state=emptyState, {type, payload}) => {
   switch(type){
-    case 'CATEGORY_CREATE':
-      return [...state, payload]
-    case 'CATEGORY_UPDATE':
-      return state.map(item => item.id === payload.id ? payload : item)
-    case 'CATEGORY_DESTROY':
-      return state.filter(item => item.id !== payload.id)
-    default:
-      return state
+  case 'CATEGORY_CREATE':
+    validateCategory(payload)
+    return [...state, payload]
+  case 'CATEGORY_UPDATE':
+    return state.map(item => item.id === payload.id ? payload : item)
+  case 'CATEGORY_DESTROY':
+    return state.filter(item => item.id !== payload.id)
+  default:
+    return state
   }
 }
